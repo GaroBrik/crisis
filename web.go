3e69962f5d38fa3ca443e52138cfc7b240ebb1be
Garo Brik <garo.brik@gmail.com>
2015-09-03 16:16:26 -0400

    git

--- web.go
+++ web.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
+	crisis.Serve()
 	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
 	fmt.Printf("listening on %s...", bind)
 	err := http.ListenAndServe(bind, nil)
 	if err != nil {
 		panic(err)
 	}
-	crisis.Serve()
 }
