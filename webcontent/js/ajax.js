3b8a97cc5e4216fcebce26d71a37cd6fd817daa0
Garo Brik <garo.brik@gmail.com>
2015-09-14 21:52:13 -0400

    fix

--- webcontent/js/ajax.js
+++ webcontent/js/ajax.js
@@ -2,5 +2,5 @@ crisis.ajax = {};
 crisis.ajax.path = "ajax/";
 /** @export */
 crisis.ajax.mapPath = crisis.ajax.path + "map/";
-crisis.ajax.modifyDivisionPath = ajaxPath + "modDiv/";
+crisis.ajax.modifyDivisionPath = crisis.ajax.path + "modDiv/";
 
