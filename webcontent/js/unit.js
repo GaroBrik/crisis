a0215cea2408ac72b5a71d96d64980caff79e335
Garo Brik <garo.brik@gmail.com>
2015-09-18 12:14:44 -0400

    fix

--- webcontent/js/unit.js
+++ webcontent/js/unit.js
@@ -1,50 +1,54 @@
-/** 
+/**
  * @constructor
- * @param{crisisJson.Unit} unitJson 
- * @param{crisis.Division} div
- */ 
+ * @param {crisisJson.Unit} unitJson
+ * @param {crisis.Division} div
+ */
 crisis.Unit = function(unitJson, div) {
     var unit = this;
-    
-    /** @type{crisis.Division} */
+
+    /** @type {crisis.Division} */
     unit.division = div;
-    /** @type{number} */
+    /** @type {number} */
     unit.amount = unitJson.Amount;
-    /** @type{string} */
+    /** @type {string} */
     unit.typeName = unitJson.TypeName;
-    /** @type{number} */
+    /** @type {number} */
     unit.typeNum = unitJson.TypeNum;
-    /** @type{jQuery} */
+    /** @type {jQuery} */
     unit.$listItem = crisis.cloneProto(crisis.$protoUnitListItem);
-    /** @type{jQuery} */
-    unit.$value = unit.$listItem.find(".value").html(unit.amount);
-    /** @type{jQuery} */
-    unit.$type = unit.$listItem.find(".type").html(crisis.Unit.typeHtml(unit.typeNum));
-    /** @type{jQuery} */
-    unit.$editField = unit.$listItem.find(".editField");
-    /** @type{jQuery} */
-    unit.$removeUnitButton = unit.$listItem.find(".removeUnitButton");
-    unit.$removeUnitButton.on("click.crisis", function() {
+    /** @type {jQuery} */
+    unit.$value = unit.$listItem.find('.value').html(unit.amount);
+    /** @type {jQuery} */
+    unit.$type = unit.$listItem.find('.type').append(
+        crisis.Unit.typeHtml(unit.typeNum));
+    /** @type {jQuery} */
+    unit.$editField = unit.$listItem.find('.editField');
+    /** @type {jQuery} */
+    unit.$removeUnitButton = unit.$listItem.find('.removeUnitButton');
+    unit.$removeUnitButton.on('click.crisis', function() {
         unit.division.details.removeUnit(unit);
     });
-    /** @type{jQuery} */
-    unit.$invalidAlert = unit.$listItem.find(".invalidAlert");
-}
+    /** @type {jQuery} */
+    unit.$invalidAlert = unit.$listItem.find('.invalidAlert');
+};
 
 crisis.Unit.prototype.editOn = function() {
     this.$editField.val(this.amount).show();
     this.$removeUnitButton.show();
     this.$value.hide();
-}
+};
 
 crisis.Unit.prototype.editOff = function() {
     this.$editField.hide();
     this.$removeUnitButton.hide();
     this.$value.show();
-}
+};
 
-/** @param{number} typeId */
+/**
+ * @param {number} typeId
+ * @return {jQuery}
+*/
 crisis.Unit.typeHtml = function(typeId) {
     return crisis.cloneProto(
-	      crisis.$protoUnitTypes.find(crisis.unitTypeSelector(typeId)));
+        crisis.$protoUnitTypes.find(crisis.unitTypeSelector(typeId)));
 }
