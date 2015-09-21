5a53a725423c641b12b86bad793f4cb19927aa99
Garo Brik <garo.brik@gmail.com>
2015-09-21 19:19:40 -0400

    fix

--- webcontent/js/map.js
+++ webcontent/js/map.js
@@ -179,8 +179,8 @@ crisis.map.zoom = function(factor) {
     map.$holder.animate({
         'height': map.bounds.height + '%',
         'width': map.bounds.width + '%',
-        'top': Math.min(crisis.getCssPx(map.$holder, 'top') + crisis.getCssPx(map.$holder, 'height') * factor / 4, 0),
-        'left': Math.min(crisis.getCssPx(map.$holder, 'left') + crisis.getCssPx(map.$holder, 'width') * factor / 4, 0)
+        'top': Math.min(crisis.getCssPx(map.$holder, 'top') + crisis.getCssPx(map.$holder, 'height') * factor / 2, 0),
+        'left': Math.min(crisis.getCssPx(map.$holder, 'left') + crisis.getCssPx(map.$holder, 'width') * factor / 2, 0)
     });
 };
 
